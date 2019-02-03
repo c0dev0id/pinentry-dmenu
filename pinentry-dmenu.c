1daeffd2f5964058f22c3d08f0e2dcad1568e87a
Quentin Rameau <quinq@fifth.space>
2019-02-04 00:29:26 +0100

    Close when the embedding window is destroyed

--- pinentry-dmenu.c
+++ pinentry-dmenu.c
@@ -362,7 +362,7 @@ setup(void)
 	XSetInputFocus(dpy, win, RevertToParent, CurrentTime);
 
 	if (embedded) {
-		XSelectInput(dpy, parentwin, FocusChangeMask);
+		XSelectInput(dpy, parentwin, FocusChangeMask | SubstructureNotifyMask);
 
 		if (XQueryTree(dpy, parentwin, &dw, &w, &dws, &du) && dws) {
 			for (i = 0; i < du && dws[i] != win; ++i) {
@@ -580,6 +580,11 @@ run(void)
 		if (XFilterEvent(&ev, None))
 			continue;
 		switch(ev.type) {
+		case DestroyNotify:
+			if (ev.xdestroywindow.window != win)
+				break;
+			cleanup();
+			exit(1);
 		case Expose:
 			if (ev.xexpose.count == 0) {
 				drw_map(drw, win, 0, 0, mw, mh);
